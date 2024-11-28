4eacc74b9d561918f3cc91f664ea9e0cdea118c1
wadoyoka <atsushi021028@gmail.com>
2024-11-28 22:29:23 +0900

    Dialog編集中

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -10,12 +10,26 @@ import { useState } from 'react'
 import { callCloudFunction } from '../utils/callCloudFunction'
 import SubmitButton from "./SubmitButton"
 import { Button } from "./ui/button"
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
 
 
 export default function ContactForm() {
   const [isLoading, setIsLoading] = useState(false)
   const { toast } = useToast()
   const [urls, setUrls] = useState<string[]>([""]);
+  const [bufUrl, setBufUrl] = useState<string>("");
+  const [titles, setTitles] =useState<string[]>([""]);
+  const [bufTitle, setBufTitle] = useState<string>("");
+
+
+  const handleAddUrl = (url:string, title:string) =>{
+    const newUrls = [...urls, url];
+    setUrls(newUrls);
+    setBufUrl("");
+    const newTitles = [...titles, title];
+    setTitles(newTitles);
+    setBufTitle("");
+  }
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -73,11 +87,59 @@ export default function ContactForm() {
             <Label htmlFor="email">メールアドレス</Label>
             <Input id="email" name="email" type="email" required />
           </div>
-          <div className="space-y-2">
+          <div className="space-y-2 flex flex-col">
             <Label htmlFor="email">URL</Label>
             {urls.map((url, index) => (
               <>
-                <Input id="email" name="email" type="email" required />
+                <Dialog key={`${url}_${index}`}>
+                  <DialogTrigger asChild>
+                    <Button variant="outline" className="text-left"><div className="flex mr-auto">{url === "" ? <p className="text-slate-400">https://example.com</p> : `${url}`}</div></Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                      <DialogTitle>URLの編集</DialogTitle>
+                      <DialogDescription>
+                        URLとURLのタイトルを入力してください。
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                      <div className="grid grid-cols-6 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                          URL
+                        </Label>
+                        <Input
+                          id={`Url_${index}`}
+                          placeholder="https://example.com"
+                          className="col-span-5"
+                          value={bufUrl}
+                          onChange={(e) => setBufUrl(e.target.value)}
+                        />
+                      </div>
+                      <div className="grid grid-cols-6 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                          Title
+                        </Label>
+                        <Input
+                          id={`Url_Title_${index}`}
+                          placeholder="タイトル"
+                          className="col-span-5"
+                          value={bufTitle}
+                          onChange={(e) => setBufTitle(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <DialogFooter className="flex">
+                      <div className="flex ml-auto gap-2">
+                        <DialogClose asChild>
+                          <Button type="button" variant="outline">
+                            キャンセル
+                          </Button>
+                        </DialogClose>
+                        <Button type="button" onClick={() =>{handleAddUrl(bufUrl, bufTitle)}}>保存</Button>
+                      </div>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
                 <Button type="button" variant={"ghost"} className="flex mx-auto my-2 bg-slate-100 w-full hover:text-sky-400">
                   <Plus />
                 </Button>
