e2e72690cbc1d747843586cc9b122f0130cdaf7e
wadoyoka <atsushi021028@gmail.com>
2024-11-30 02:25:57 +0900

    チョビ変更

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -18,11 +18,11 @@ export default function ContactForm() {
   const { toast } = useToast()
   const [urls, setUrls] = useState<string[]>([""]);
   const [bufUrl, setBufUrl] = useState<string>("");
-  const [titles, setTitles] =useState<string[]>([""]);
+  const [titles, setTitles] = useState<string[]>([""]);
   const [bufTitle, setBufTitle] = useState<string>("");
 
 
-  const handleAddUrl = (url:string, title:string) =>{
+  const handleAddUrl = (url: string, title: string) => {
     const newUrls = [...urls, url];
     setUrls(newUrls);
     setBufUrl("");
@@ -90,61 +90,59 @@ export default function ContactForm() {
           <div className="space-y-2 flex flex-col">
             <Label htmlFor="email">URL</Label>
             {urls.map((url, index) => (
-              <>
-                <Dialog key={`${url}_${index}`}>
-                  <DialogTrigger asChild>
-                    <Button variant="outline" className="text-left"><div className="flex mr-auto">{url === "" ? <p className="text-slate-400">https://example.com</p> : `${url}`}</div></Button>
-                  </DialogTrigger>
-                  <DialogContent className="sm:max-w-[425px]">
-                    <DialogHeader>
-                      <DialogTitle>URLの編集</DialogTitle>
-                      <DialogDescription>
-                        URLとURLのタイトルを入力してください。
-                      </DialogDescription>
-                    </DialogHeader>
-                    <div className="grid gap-4 py-4">
-                      <div className="grid grid-cols-6 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                          URL
-                        </Label>
-                        <Input
-                          id={`Url_${index}`}
-                          placeholder="https://example.com"
-                          className="col-span-5"
-                          value={bufUrl}
-                          onChange={(e) => setBufUrl(e.target.value)}
-                        />
-                      </div>
-                      <div className="grid grid-cols-6 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
-                          Title
-                        </Label>
-                        <Input
-                          id={`Url_Title_${index}`}
-                          placeholder="タイトル"
-                          className="col-span-5"
-                          value={bufTitle}
-                          onChange={(e) => setBufTitle(e.target.value)}
-                        />
-                      </div>
+              <Dialog key={`${url}_${index}`}>
+                <DialogTrigger asChild>
+                  <Button variant="outline" className="text-left"><div className="flex mr-auto">{url === "" ? <p className="text-slate-400">https://example.com</p> : `${url}`}</div></Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>URLの編集</DialogTitle>
+                    <DialogDescription>
+                      URLとURLのタイトルを入力してください。
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-6 items-center gap-4">
+                      <Label htmlFor="name" className="text-right">
+                        URL
+                      </Label>
+                      <Input
+                        id={`Url_${index}`}
+                        placeholder="https://example.com"
+                        className="col-span-5"
+                        value={bufUrl}
+                        onChange={(e) => setBufUrl(e.target.value)}
+                      />
                     </div>
-                    <DialogFooter className="flex">
-                      <div className="flex ml-auto gap-2">
-                        <DialogClose asChild>
-                          <Button type="button" variant="outline">
-                            キャンセル
-                          </Button>
-                        </DialogClose>
-                        <Button type="button" onClick={() =>{handleAddUrl(bufUrl, bufTitle)}}>保存</Button>
-                      </div>
-                    </DialogFooter>
-                  </DialogContent>
-                </Dialog>
-                <Button type="button" variant={"ghost"} className="flex mx-auto my-2 bg-slate-100 w-full hover:text-sky-400">
-                  <Plus />
-                </Button>
-              </>
+                    <div className="grid grid-cols-6 items-center gap-4">
+                      <Label htmlFor="username" className="text-right">
+                        Title
+                      </Label>
+                      <Input
+                        id={`Url_Title_${index}`}
+                        placeholder="タイトル"
+                        className="col-span-5"
+                        value={bufTitle}
+                        onChange={(e) => setBufTitle(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <DialogFooter className="flex">
+                    <div className="flex ml-auto gap-2">
+                      <DialogClose asChild>
+                        <Button type="button" variant="outline">
+                          キャンセル
+                        </Button>
+                      </DialogClose>
+                      <Button type="button" onClick={() => { handleAddUrl(bufUrl, bufTitle) }}>保存</Button>
+                    </div>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
             ))}
+            <Button type="button" variant={"ghost"} className="flex mx-auto my-2 bg-slate-100 w-full hover:text-sky-400">
+              <Plus />
+            </Button>
           </div>
           <div className="space-y-2">
             <Label htmlFor="message">メモ内容</Label>
